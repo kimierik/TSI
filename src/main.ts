4cd18f0009b14f8ffc79df7c569cb30b5fa8f03d
kimierik <kimi.malkamaki@gmail.com>
2023-06-18 09:02:26 +0300

    scuffed way to choose what files to execute

--- src/main.ts
+++ src/main.ts
@@ -7,15 +7,51 @@ import * as path from 'path';
 import { Parser } from "./compiler/parser";
 import { prettyPrintAst, prettyPrintByteCode } from "./utils/Prettyprint";
 import { ByteCodeCompiler } from "./compiler/bytecode-compiler";
+import {exit} from "process";
 
 
 function main(){
     console.log("main start")
+    let filename=""
+
+    console.log(process.argv.length)
+
+    if (process.argv.length==1){
+        printHelp()
+        exit()
+    }
+    if (process.argv.length==3){
+        filename=process.argv[2]
+    }
+
+    for (let i =0; i<process.argv.length;i++){
+        let arg=process.argv[i]
+        switch(arg){
+            case '-h':
+                printHelp()
+            break
+            
+            case '--help':
+                printHelp()
+            break
+
+            case '-i':
+                filename=process.argv[i+1]
+                break
+        }
+    }
+
+    if (filename==""){
+        printHelp()
+        exit()
+    }
+
+
     let parser=new Parser;
     let compiler = new ByteCodeCompiler()
 
-    let data= fs.readFileSync(path.join(__dirname,"../examples/factorial.foo") )
-    //let data= fs.readFileSync(path.join(__dirname,"../examples/test.foo") )
+    let rpath = path.join(__dirname,"../"+filename) 
+    let data= fs.readFileSync(rpath)
 
     let tokens=lexInput(data.toString())
     
@@ -40,3 +76,11 @@ function main(){
 
 
 main()
+
+function printHelp(){
+    console.log("help text")
+    console.log("first argument should be the filename to interpret")
+
+}
+
+
